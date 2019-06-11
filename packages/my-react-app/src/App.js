947359daeed607b2bb08def72246e6c0cfb2ebca
Нартай Дембаев <mastaok02@gmail.com>
2019-06-11 17:30:06 +0600

    component button added

---
+++ packages/my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CompButton from "@project/comp-button"
+
+function App() {
+  return (
+    <CompButton/>
+  );
+}
+
+export default App;
